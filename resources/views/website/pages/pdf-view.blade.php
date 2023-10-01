9162c899299b9f42ad0ba8e699accb851a2761b0
MH-Shakil38 <maynuddinhsn@gmail.com>
2023-10-01 14:29:45 +0600

    .....

--- resources/views/website/pages/pdf-view.blade.php
+++ resources/views/website/pages/pdf-view.blade.php
@@ -1,7 +1,7 @@
 @extends('website.layouts.schoolMaster')
 @section('meta')
     <meta property="og:url" content="{{route('notice.details',$details->id)}}" />
-    <meta property="og:type" content="article" />
+    <meta property="og:type" content="notice" />
     <meta property="og:title" content="{{$details->title}}" />
     <meta property="og:description" content="{!! $details->details ?? '' !!}" />
     <meta property="og:image" content="{{asset(setting()->logo)}}" />
